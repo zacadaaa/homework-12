8d34e537de328ba297122812d507ea5f15b17f4c
kushedow <kushedow@gmail.com>
2021-11-29 01:50:55 +0300

    restored

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, send_from_directory
-from functions import get_tags_from_posts_file, get_posts_by_tag,  upload_image, save_post
+# from functions import ...
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,36 +9,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    tags = get_tags_from_posts_file(POST_PATH)
-    return render_template("index.html", tags=tags)
+    pass
 
 
 @app.route("/tag")
 def page_tag():
-
-    tag = request.values.get("tag")
-
-    if tag:
-        posts_match = get_posts_by_tag(tag, POST_PATH)
-        return render_template("post_by_tag.html", tag=tag, posts=posts_match)
-    return "тэг не задан"
-
+    pass
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
-
-    if request. method=="POST":
-        picture, content = request.files.get("picture"), request.form.get("content")
-
-        if picture and content:
-            picture_url = upload_image(picture, UPLOAD_FOLDER)
-            post_data = {"pic": "/"+picture_url, "content": content}
-            save_post(POST_PATH, post_data)
-
-            return render_template("post_uploaded.html", post_data=post_data)
-        return "Ошибка загрузки "
-    return render_template("post_form.html")
+    pass
 
 
 @app.route("/uploads/<path:path>")
