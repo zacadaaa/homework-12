e99a97b64c57e90eb57d4210703e420ca8c43aff
zacadaaa <styopanikonov@yandex.ru>
2022-09-21 01:05:58 +0500

    ...

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+import logging
+
+from loader.views import loader_blueprint
+from main.views import main_blueprint
+
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -7,24 +11,10 @@ UPLOAD_FOLDER = "uploads/images"
 app = Flask(__name__)
 
 
-@app.route("/")
-def page_index():
-    pass
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
 
-
-@app.route("/list")
-def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_form():
-    pass
-
-
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
+logging.basicConfig(filename="basic.log", level=logging.INFO)
 
 
 @app.route("/uploads/<path:path>")
@@ -32,5 +22,5 @@ def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run()
+app.run(debug=True)
 
--- functions.py
+++ functions.py
@@ -1,1 +1,25 @@
 import json
+
+
+def load_posts() -> list[dict]:
+    """Чтение файла json"""
+    with open('posts.json', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def get_posts_by_word(word) -> list[dict] :
+    result = []
+    for post in load_posts():
+        if word.lower() in post['content'].lower():
+            result.append(post)
+    return result
+
+
+def add_post(post: dict) -> dict:
+    posts: list[dict] = load_posts()
+    posts.append(post)
+    with open('posts.json', 'w', encoding='utf-8') as file:
+        json.dump(posts, file)
+    return post
+
+
